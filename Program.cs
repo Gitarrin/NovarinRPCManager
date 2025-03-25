a34a947ee9ac385ad4e2f4cb77d522c64bc97008
Loganh4005 <60761520+Loganh4005@users.noreply.github.com>
2025-03-25 11:34:15 -0600

    Revert "i hate .net core now why is the binary 68mb!"
    This reverts commit c6716ac8fd616a35ad191dffabe0c9aac23b5cfc.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
 using CommandLine;
 using Discord;
 using Newtonsoft.Json;
